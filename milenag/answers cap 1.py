8c0e0f50299990b7eba972e509900451b83563db
Hypat-ia <milenagu09@gmail.com>
2020-04-28 19:40:50 -0500

    Ejercicios cap 1

---
+++ milenag/answers cap 1.py
@@ -0,0 +1,20 @@
+print ("""EJERCICIOS PYTHON
+PRIMER CAPITULO:
+1 c Almacenar información durante mucho tiempo, incluso después de ciclos de
+apagado y encendido 
+2 Instrucciones que indican como realizar cualquier cosa 
+3 El compilador necesita el codigo hecho para ejecutar, mas que el interprete analiza el codigo y lo entiende
+4 Cuál de los siguientes contiene “código máquina”
+b El teclado
+5 * primt * -> print
+6 a Unidad central de procesamiento
+7 a 43
+
+8 Explica cada uno de los siguientes conceptos usando un ejemplo de una capacidad humana:
+1 Unidad central de procesamiento : el cerebro procesando informacion todo el tiempo
+2 Memoria principal : la memoria a corto plazo del cerebro 
+3 Memoria secundaria : hipocampo del cerebro en el que se guardas memorias consolidadas  
+4 Dispositivos de entrada : los sentidos 
+5 Dispositivos de salida : hablar
+
+9 Revisando y analizando el código, posteriormente mirar el error de redacción del código y corregirlo""")
